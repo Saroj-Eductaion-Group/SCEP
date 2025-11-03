66babc92eff048e83efc39efd94a87fff62bfae8
vkassingh <44vikasrajput44@gmail.com>
2025-11-03 10:32:15 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  preview: {
-    allowedHosts: ['saroj-college-engineering-polytechnic.onrender.com']
-  }
+   base: '/scep/',
 })
